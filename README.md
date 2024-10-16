a59dd06d2db7595f441f307369b235136d37714b
KAMAL-02 <Kamalnayan403@gmail.com>
2024-10-16 14:08:45 +0530

    feat: Add projects

---
+++ README.md
@@ -0,0 +1,13 @@
+# Kamal's Portfolio
+
+**Vortex**! my personalised portfolio
+
+## 📸 Preview
+
+![Portfolio Preview](./public/images/preview.png)
+
+## 🔗 Live Demo
+
+- **Live Demo**: [Visit my Portfolio]('')
+
+Thank you for visiting my portfolio!
---
+++ src/app/(shared)/work/page.tsx
@@ -0,0 +1,85 @@
+// import CdBack from '@/components/shared/CdBack'
+// import { ArrowUpRight, ChevronRight } from 'lucide-react'
+// import type { Metadata } from 'next'
+// import Link from 'next/link'
+
+// export const metadata: Metadata = {
+//   title: 'Work',
+//   description: 'A Snapshot of my work and contributions.',
+// }
+
+// export default function WorkPage() {
+//   return (
+//     <section className="py-5 dark:text-zinc-200 md:mt-8">
+//       <div className="mb-8 flex items-center justify-between">
+//         <h1 className="text-3xl font-bold tracking-tight">my work</h1>
+
+//         <Link
+//           href=""
+//           target="_blank"
+//           className="flex items-center justify-around gap-3 rounded-xl border-2 border-dashed border-black bg-white px-4 py-2 text-sm font-semibold text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-none hover:shadow-[4px_4px_0px_black] active:translate-x-0 active:translate-y-0 active:rounded-2xl active:shadow-none dark:border-white dark:bg-black dark:text-gray-400 dark:hover:shadow-[4px_4px_0px_white]"
+//         >
+//           View Resume
+//           <ChevronRight className="size-5 stroke-black dark:stroke-gray-400" />
+//         </Link>
+//       </div>
+//       <div className="space-y-5">
+//         {' '}
+//         <p>
+//           soon
+//         </p>
+//         <p>Here &apos;s a snapshot of my journey so far.</p>
+//       </div>
+
+//       <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+
+//       <div className="space-y-8">
+//         <div className="space-y-2">
+//           <Link
+//             href={''}
+//             target="_blank"
+//             className="group flex items-center justify-start gap-3 overflow-hidden"
+//           >
+//             <h2 className="text-2xl font-semibold">Leadlly .</h2>
+
+//             <div className="relative size-5 -translate-x-4 -translate-y-1 overflow-hidden">
+//               <ArrowUpRight className="size-5 transition-all duration-300 ease-in-out group-hover:-translate-y-full group-hover:translate-x-full" />
+//               <ArrowUpRight className="relative size-5 -translate-x-full transition-all duration-300 ease-in-out group-hover:-translate-y-full group-hover:translate-x-0" />
+//             </div>
+//           </Link>
+//           <p className="text-sm text-neutral-600 dark:text-neutral-400">
+//             Full Stack Developer | April &apos;24 - Present
+//           </p>
+//         </div>
+
+//         <p className="leading-relaxed">
+//           soon
+//         </p>
+//         <ul className="list-inside list-disc space-y-4 leading-relaxed">
+//           <li>
+//            soon
+//           </li>
+//           <li>
+//             soon
+//             <ul className="ml-6 list-inside list-disc">
+//               <li></li>
+//               <li></li>
+//               <li></li>
+//             </ul>
+//           </li>
+//         </ul>
+//         <p className="mt-6 leading-relaxed">
+//           soon
+//         </p>
+//       </div>
+
+//       <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+
+//       <p className="mt-6 leading-relaxed">
+//         soon
+//       </p>
+
+//       <CdBack />
+//     </section>
+//   )
+// }
